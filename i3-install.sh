a6ca1acb3a8c007714183bcd081e7672a22461e8
JoaoPedroPierri <joaopedropierri2002@gmail.com>
2021-02-28 16:30:21 +0000

    update

--- i3-install.sh
+++ i3-install.sh
@@ -25,7 +25,7 @@ sudo pacman -S reflector --noconfirm
 sudo reflector -n 5 --save /etc/pacman.d/mirrorlist
 
 # Baixando pacotes
-clear && sudo pacman -S git wget curl xorg-xrandr xclip usbutils wget pulseaudio-alsa pavucontrol qbittorrent brightnessctl dunst numlockx mpv libnotify viewnior python-pip pacman-contrib gvfs-mtp android-tools w3m flameshot neofetch nitrogen udev zsh zsh-syntax-highlighting redshift lib32-dbus-glib imagemagick jq lsd base-devel --noconfirm 
+clear && sudo pacman -S git wget curl xorg-xrandr xclip unzip usbutils wget pulseaudio-alsa pavucontrol qbittorrent brightnessctl dunst numlockx mpv libnotify viewnior python-pip pacman-contrib gvfs-mtp android-tools w3m flameshot neofetch nitrogen udev zsh zsh-syntax-highlighting redshift lib32-dbus-glib imagemagick jq lsd base-devel --noconfirm 
 
 # Limpar console
 
